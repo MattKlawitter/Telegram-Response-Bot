7fcd5aef6a04d86e78c01ebf6d8322852c2e8186
Matthew Klawitter <mtklaw153@gmail.com>
2019-09-12 17:06:26 -0400

    Disabled CLI again

--- src/start.py
+++ src/start.py
@@ -92,17 +92,22 @@ command = ""
 logger.info("Command-line enabled:")
 
 # TODO: Implement cli in another thread
-while not command == "quit":
-    command = input()
+# while not command == "quit":
+#     command = input()
 
+#     if not thread.is_alive():
+#         if command == "restart":
+#             logger.info("Restarting the bot...")
+#             thread.start()
+
+#     if command == "stop":
+#         logger.warning("Stopping the bot...")
+#         thread.stop()
+while True:
     if not thread.is_alive():
-        if command == "restart":
-            logger.info("Restarting the bot...")
-            thread.start()
-
-    if command == "stop":
-        logger.warning("Stopping the bot...")
-        thread.stop()
+        logger.warning("Bot died, attempting to restart...")
+        thread.start()
+    time.sleep(10)
 
 # Close bot thread
 thread.stop()
